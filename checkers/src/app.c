fc66f8af96082a59065992757db8bea9fd52ec57
szyszoch <pyrun97531@gmail.com>
2022-11-11 17:12:50 +0100

    Adding textures [1/2]

--- checkers/src/app.c
+++ checkers/src/app.c
@@ -1,15 +1,27 @@
 #include "app.h"
 
+enum TEXTURES {
+	TEXTURE_WHITE_CHECKER,
+	TEXTURE_BLACK_CHECKER,
+	TEXTURE_WHITE_KING_CHECKER,
+	TEXTURE_BLACK_KING_CHECKER,
+	TEXTURE_BOARD,
+	TEXTURE_SELECT,
+	TEXTURES_COUNT
+};
+
 typedef struct App {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Event* event;
+	SDL_Texture* texture[TEXTURES_COUNT];
 } App;
 
 static App app;
 
 int App_Init() {
 
+	// Initializing libraries
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("%s\n", SDL_GetError());
 		return -1;
@@ -20,22 +32,43 @@ int App_Init() {
 		return -2;
 	}
 
+	// Creating window
 	app.window = SDL_CreateWindow(APP_WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, APP_WINDOW_WIDTH, APP_WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	if (app.window == NULL) {
 		printf("%s\n", SDL_GetError());
 		return -3;
 	}
 
+	// Creating renderer
 	app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
 	if (app.renderer == NULL) {
 		printf("%s\n", SDL_GetError());
 		return -4;
 	}
 
+	// Loading textures
+	app.texture[TEXTURE_WHITE_CHECKER] = IMG_LoadTexture(app.renderer, "img/white_checker.png");
+	app.texture[TEXTURE_BLACK_CHECKER] = IMG_LoadTexture(app.renderer, "img/black_checker.png");
+	app.texture[TEXTURE_WHITE_KING_CHECKER] = IMG_LoadTexture(app.renderer, "img/white_king_checker.png");
+	app.texture[TEXTURE_BLACK_KING_CHECKER] = IMG_LoadTexture(app.renderer, "img/black_king_checker.png");
+	app.texture[TEXTURE_BOARD] = IMG_LoadTexture(app.renderer, "img/board.png");
+	app.texture[TEXTURE_SELECT] = IMG_LoadTexture(app.renderer, "img/select.png");
+
+	for (int i = 0; i < TEXTURES_COUNT; i++) {
+		if (app.texture[i] == NULL) {
+			printf("%s\n", SDL_GetError());
+			return -5;
+		}
+	}
+
 	return 0;
 }
 
 void App_Destroy() {
+
+	for (int i = 0; i < TEXTURES_COUNT; i++) {
+		SDL_DestroyTexture(app.texture[i]);
+	}
 
 	SDL_DestroyRenderer(app.renderer);
 	SDL_DestroyWindow(app.window);
@@ -66,3 +99,4 @@ int App_Game() {
 int App_GameOver() {
 	return 0;
 }
+
